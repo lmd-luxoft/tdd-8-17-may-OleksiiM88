e0b8d3e0c019bda58d0dc85e78c79856b8e53888
Oleksii Maievskyi <oleksii.maievskyi@dxc.com>
2023-05-10 11:06:21 +0300

    Initial commit

---
+++ TDDKata/CalcTests.cs
@@ -0,0 +1,130 @@
+using System.Reflection.Metadata;
+using TDDKataCalc;
+
+namespace TDDKata
+{
+	public class CalcTests
+	{
+		private readonly Calc _calc;
+        public CalcTests()
+        {
+            _calc = new Calc();
+        }
+
+        [Fact]
+		public void ReturnZeroAsEmptyInputString()
+		{
+			//Arrange
+			int expected = 0;
+
+			//Act
+			decimal actual = _calc.Add(String.Empty);
+
+			//Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ReturnZeroAsNullInputString()
+		{
+			//Arrange
+			int expected = 0;
+
+			//Act
+			decimal actual = _calc.Add(digits: null);
+
+			//Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void PassOnlyOneParameter()
+		{
+			//Arrange
+			string parameter = "1";
+			int expected = 1;
+
+			//Act
+			decimal actual = _calc.Add(parameter);
+
+			//Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void PassOnlyOneParameterButNotDigit()
+		{
+			//Arrange
+			string parameter = "T";
+
+			//Act & Assert
+			Assert.Throws<InvalidOperationException>(() => _calc.Add(parameter));
+		}
+
+		[Fact]
+		public void PassTwoParametersAsDigits()
+		{
+			//Arrange
+			string parameters = "1,2";
+			int expected = 3;
+
+			//Act
+			decimal actual = _calc.Add(parameters);
+
+			//Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void PassMoreThenTwoParametersAsDigits()
+		{
+			//Arrange
+			string parameters = "1,24,15";
+			int expected = 40;
+
+			//Act
+			decimal actual = _calc.Add(parameters);
+
+			//Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void PassFloatsParametersAsDigits()
+		{
+			//Arrange
+			string parameters = "1.5,2.6";
+			decimal expected = 4.1m;
+
+			//Act
+			decimal actual = _calc.Add(parameters);
+
+			//Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void PassCharAndDigitAsParameters()
+		{
+			//Arrange
+			string parameters = "#,2.6";
+
+			//Act & Assert
+			Assert.Throws<InvalidOperationException>(() => _calc.Add(parameters));
+		}
+
+		[Fact]
+		public void PassNegativeParametersAsDigits()
+		{
+			//Arrange
+			string parameters = "-45,-2";
+			decimal expected = -47;
+
+			//Act
+			decimal actual = _calc.Add(parameters);
+
+			//Assert
+			Assert.Equal(expected, actual);
+		}
+	}
+}
---
+++ TDDKataCalc/Calc.cs
@@ -0,0 +1,39 @@
+﻿namespace TDDKataCalc
+{
+	public class Calc
+	{
+		public decimal Add(string digits)
+		{
+			if(String.IsNullOrEmpty(digits))
+				return 0;
+			
+			if (digits.Length == 1)
+			{
+				return ValueParser(digits);
+				
+			}
+
+			string[] values = digits.Split(',');
+			decimal result = 0;
+			foreach(string value in values)
+			{
+				result += ValueParser(value);
+			}
+			
+			return result;
+			
+		}
+
+		private decimal ValueParser(string value)
+		{
+			if (decimal.TryParse(value, out decimal digit))
+			{
+				return digit;
+			}
+			else
+			{
+				throw new InvalidOperationException("Value can't be parsed");
+			}
+		}
+	}
+}
