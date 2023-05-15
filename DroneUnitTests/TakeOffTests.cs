7f4a425093510c5d32af4eb94f19b0b7a48558d3
Oleksii Maievskyi <oleksii.maievskyi@dxc.com>
2023-05-15 11:56:21 +0300

    Added TakeOffTests

---
+++ DroneUnitTests/TakeOffTests.cs
@@ -0,0 +1,42 @@
+using Drone;
+using Drone.Exceptions;
+
+namespace DroneUnitTests
+{
+	public class TakeOffTests : IDisposable
+	{
+        private readonly Quadrocopter _quadrocopter;
+
+        public TakeOffTests()
+        {
+			var controller = new FlyController();
+			var gps = new GpsModule();
+			var compass = new Compass();
+			_quadrocopter = new Quadrocopter(compass, gps, controller);
+		}
+
+        [Fact]
+		public void TakeOffNotAvailableIfBadGpsSignal()
+		{
+			Assert.Throws<GpsException>(() => _quadrocopter.TakeOff());
+		}
+
+		[Fact]
+		public void TakeOffNotAvailableIfCompassNotCalibration()
+		{
+			Assert.Throws<CompassException>(() => _quadrocopter.TakeOff());
+		}
+
+		[Fact]
+		public void NormalTakeoff()
+		{
+			_quadrocopter.TakeOff();
+		}
+
+		public void Dispose()
+		{
+
+			//TODO as TearDown!
+		}
+	}
+}
